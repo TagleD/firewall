81f83a16b1a587943d9739fc773eb8ae4f2410e3
GLeb Tarakanov <glebtgd@gmail.com>
2025-03-13 01:02:43 +0500

    Добавлена возможность входа в аккаунт, регистрации и лендинг

--- source/account/urls.py
+++ source/account/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from webapp import views
+from account import views
 
 
 urlpatterns = [
-    # Главная
-    # path('', views.IndexView.as_view(), name='index'),
+    # auth and registration urls
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('logout/', views.logout_view, name='logout'),
+
+# user profile and edit urls
+    path('profile/<int:pk>/', views.UserDetailView.as_view(), name='user_detail'),
+    path('profile/<int:pk>/update/', views.UserUpdateView.as_view(), name='user_update'),
 ]
---
+++ source/account/views/__init__.py
@@ -0,0 +1,1 @@
+from .user import LoginView, RegisterView, logout_view, UserUpdateView, UserDetailView
