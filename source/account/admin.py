78ac7438ff38723a43bb0ed50eb9b59eb83257a6
GLeb Tarakanov <glebtgd@gmail.com>
2025-03-24 14:55:55 +0500

    добавлена админка

--- source/account/admin.py
+++ source/account/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+
 
 # Register your models here.
+@admin.register(get_user_model())
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'is_superuser',
+    )
+    list_display_links = (
+        'id',
+        'username',
+        'is_superuser',
+    )
--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'name',
+        'created_at',
+    )
+    list_display_links = (
+        'id',
+        'user',
+        'name',
+        'created_at',
+    )
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'report',
+        'time',
+        'amount',
+        'is_fraud',
+        'explanation',
+        'transaction_varchar_id',
+    )
+    list_display_links = (
+        'id',
+        'report',
+        'time',
+        'amount',
+        'is_fraud',
+        'explanation',
+        'transaction_varchar_id',
+    )
